6731f77ae3ab6474d75cc6f0bf54b8c2875fbfd1
emanuelecalvisi <emanuele.calvisi@gmail.com>
2022-06-14 11:35:10 +0200

    first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/code.module';
+import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    CoreModule,
+    SharedModule,
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
