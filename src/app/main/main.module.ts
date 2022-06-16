62c46f99581f1abdd5c8c1811daa227201b4e037
emanuelecalvisi <emanuele.calvisi@gmail.com>
2022-06-16 09:46:25 +0200

    ref

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { ListComponent } from './list/list.component';
-import { PostComponent } from './post/post.component';
+import { ListComponent } from '../shared/components/list/list.component';
+import { PostComponent } from '../shared/components/post/post.component';
 import { DetailComponent } from './detail/detail.component';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
